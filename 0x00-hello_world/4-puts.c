1e1fe038ecc8cdf33f6ef7114e750ed38a804889
kamaudavid <kamauneems@gmail.com>
2022-03-10 23:21:54 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+*main-where the execution begins
+*return 0 if the program finished executing successful
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
