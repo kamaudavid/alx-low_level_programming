782a1e66d37bba61d782b74a1fa0a22d954f13a4
kamaudavid <kamauneems@gmail.com>
2022-03-10 23:23:35 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main-where the execution begins
-*return 0 if the program finished executing successful
+*Return : 0 if the program finished executing successful
 */
 int main(void)
 {
