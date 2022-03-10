6d9c2211da69c3c7fd911665de9b6e8344f31123
kamaudavid <kamauneems@gmail.com>
2022-03-10 23:30:22 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+* main - it prints with proper grammar, but the outcome is a piece of art,
+*
+* Return - 0 if executed successful,non-zero otherwise
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
