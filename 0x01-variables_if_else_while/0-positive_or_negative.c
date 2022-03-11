e375c425978750975244931b4020c65c057a8603
kamaudavid <kamauneems@gmail.com>
2022-03-11 16:39:26 +0300

    changet the brackets positioning

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -13,9 +15,9 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	printf("%d is positive\n", n);
-	else if ( n == 0 )
+	else if (n == 0 )
 	printf("%d is zero\n", n);
-	else if ( n < 0)
+	else if (n < 0)
 	printf("%d is negative\n", n);
 	return (0);
 }
